af4bde3b9dac1c228587df5519a7c83c341ade90
Jon Pugh <jon@thinkdrop.net>
2015-03-22 13:37:37 -0400

    Adding "App" model.

---
+++ src/DevShop/Model/App.php
@@ -0,0 +1,45 @@
+<?php
+namespace DevShop\Model;
+
+/**
+ * An app, website, project... whatever you call the thing you are working on.
+ */
+class App {
+
+  /**
+   * @var integer
+   */
+  protected $id;
+
+  /**
+   * @var string
+   * The app's machine name.  Must be unique.
+   */
+  public $name;
+
+  /**
+   * @var string
+   * A description or title of this app.
+   */
+  public $description;
+
+  /**
+   * @var string
+   * Source code URL.
+   */
+  public $source_url;
+
+  /**
+   * @var Environment[]
+   */
+  public $environments;
+
+  /**
+   * Initiate the app object.
+   */
+  public function __construct($name, $source_url, $description = NULL) {
+    $this->name = $name;
+    $this->description = $description;
+    $this->source_url = $source_url;
+  }
+}
