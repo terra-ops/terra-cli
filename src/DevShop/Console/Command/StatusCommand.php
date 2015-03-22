409555a9157c131482a6cc97ae14175364065788
Jon Pugh <jon@thinkdrop.net>
2015-03-22 12:06:39 -0400

    Initial status command.

---
+++ src/DevShop/Console/Command/StatusCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DevShop\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StatusCommand extends Command
+{
+  protected function configure()
+  {
+    $this
+      ->setName('status')
+      ->setDescription('Display the current status.')
+      ->addArgument(
+        'server',
+        InputArgument::OPTIONAL,
+        'Which server?'
+      )
+    ;
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $name = $input->getArgument('server');
+    if (!$name) {
+      $name = 'localhost';
+    }
+    $output->writeln("Hello $name...");
+  }
+}
