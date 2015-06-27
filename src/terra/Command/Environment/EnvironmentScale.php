a768cf4bc5170d59f2784e77b0ad21812c2230ca
Jon Pugh <jon@thinkdrop.net>
2015-06-26 23:45:50 -0400

    missing ChoiceQuestion

--- src/terra/Command/Environment/EnvironmentScale.php
+++ src/terra/Command/Environment/EnvironmentScale.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Process\Process;
 use terra\Factory\EnvironmentFactory;
 
