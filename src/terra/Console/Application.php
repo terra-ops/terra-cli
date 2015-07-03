3e8d41039039d4bfacc695051bb847629a95068f
Jon Pugh <jon@thinkdrop.net>
2015-07-03 14:09:07 -0400

    Adding "environment:test" command.

--- src/terra/Console/Application.php
+++ src/terra/Console/Application.php
@@ -52,6 +52,7 @@ class Application extends BaseApplication {
     $commands[] = new Command\Environment\EnvironmentScale();
     $commands[] = new Command\Environment\EnvironmentStatus();
     $commands[] = new Command\Environment\EnvironmentProxyEnable();
+    $commands[] = new Command\Environment\EnvironmentTest();
     $commands[] = new Command\Status();
     return $commands;
   }
