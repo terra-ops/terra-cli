a62bf12f2eb3fb33cb3f2b6c896b0bc5199ce5fc
Jon Pugh <jon@thinkdrop.net>
2015-07-06 11:43:19 -0400

    Assign the URL proxy a name so we can retrieve it later.

--- src/terra/Command/Environment/EnvironmentProxyEnable.php
+++ src/terra/Command/Environment/EnvironmentProxyEnable.php
@@ -26,7 +26,7 @@ class EnvironmentProxyEnable extends Command
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     $output->writeln("Hello Terra!");
-    $cmd = 'docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy';
+    $cmd = 'docker run -d -p 80:80 --name terra_proxy -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy';
 
     // Confirm removal of the app.
     $helper = $this->getHelper('question');
@@ -38,7 +38,7 @@ class EnvironmentProxyEnable extends Command
     else {
 
       // We must use the jwilder/nginx-proxy image to allow multiple URLs per docker host.
-      $process = new Process('docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy');
+      $process = new Process($cmd);
       $process->run(function ($type, $buffer) {
         if (Process::ERR === $type) {
           echo 'DOCKER > ' . $buffer;
