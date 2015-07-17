ed94331e9ca7942fefa6d33fc0680794f75a61ff
Jon Pugh <jon@thinkdrop.net>
2015-07-16 20:52:33 -0400

    Ability to go straight to creating an environment after creating an app.

--- src/terra/Command/App/AppAdd.php
+++ src/terra/Command/App/AppAdd.php
@@ -5,8 +5,10 @@ namespace terra\Command\App;
 use terra\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class AppAdd extends Command
 {
@@ -48,6 +50,7 @@ class AppAdd extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $helper = $this->getHelper('question');
 
         // Questions.
         $name_question = new Question('System name of your project? ', '');
@@ -80,6 +83,20 @@ class AppAdd extends Command
             $output->writeln('<info>App saved</info>');
         } else {
             $output->writeln('<error>App not saved!</error>');
+        }
+
+
+        // Offer to enable the environment
+        $question = new ConfirmationQuestion("Create an environment? [y\N] ", false);
+        if ($input->getOption('create-environment') || $helper->ask($input, $output, $question)) {
+
+          // Run environment:add command.
+          $command = $this->getApplication()->find('environment:add');
+          $arguments = array(
+            'app_name' => $app['name'],
+          );
+          $input = new ArrayInput($arguments);
+          $command->run($input, $output);
         }
     }
 }
